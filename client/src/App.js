49e3589288fba0b68ec35bc1445dec824f1bd88b
Francis Vila <fvila@axway.com>
2020-05-16 17:45:50 +0200

    error

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,54 @@ import axios from "axios";
 
 class App extends Component {
   state = {
-    welcome: "..."
+    welcome: "....",
+    userList: [{name:"DDD"} ,{name:"VV"}],
+    toto:1
   };
+  
 
   componentDidMount = async () => {
     try {
       const res = await axios.get("/welcome");
-      console.log(res);
-      this.setState({ welcome: res.data });
-    } catch (error) {
+      console.log("componentDidMount res=" + res);
+      /*
+      const res2 = await axios.get("/toto");
+      console.log("componentDidMount res2=" +res2);
+      this.setState({ welcome:   res.data});
+      this.setState({ userList:   res2.data.users});
+      console.log("componentDidMount res2.data[0]=" +res2.data.users[0]);
+      console.log("componentDidMount res2=" +res2);
+      */
+    } catch (error) { 
+
       console.log(error);
     }
   };
+  
+  
+
+
+
   render() {
+    function LiList (props) {
+      return props.data.map(item =>  <li key={item.name}> {item.name} </li>)
+    }
     return (
       <div className="App">
-        <h1>"Hello server SSSS !" says the client</h1>
-        <h1>"{this.state.welcome}" says the server</h1>
+      <form method="post" action="/toto">
+      <label>Enter User Name ss </label><br/>
+      <input type="text" name="name" placeholder="Enter user name..." required />
+      <input type="submit" value="Add Name" />
+      </form>
+
+<p>yyy</p>
+<ul><LiList data={this.state.userList} /></ul>
+<p>RRR</p>
+        <h1>"Hello server SSS !" says the client</h1>
+        <h1>"{this.state.welcome}" says the server FF</h1>
+        
+        <input name='button'  type='button'  value="toto"  onClick={(e) => { this.setState({ toto: this.state.toto+1}) }} />
+        <p>{this.state.toto}</p>
       </div>
     );
   }
--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,12 @@
+/*
+variables/ids related to database connection:
+mongoose, port, options, mongoUri, conn, db, Schema, userSchema
+variables related to express server
+express, app,
+variables related to objects
+User, 
+*/ 
+
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -39,13 +48,45 @@ db.once("open", () => console.log("Successfully connected to mongo"));
 
 // Setup routes to respond to client
 app.get("/welcome", async (req, res) => {
-  console.log("Client request received");
+  try {
+    
+    console.log("Client request received");
   const user = await User.find().exec();
   console.log(user[0].name);
   res.send(
-    `Hello Client! There is one record in the database for ${user[0].name}`
-  );
+    `Hello Client! There is one record in the database for ${user[0].name} GGG`
+  ); 
+    
+  } catch (error) {
+    res.send("error: " + error.message)
+    
+  }
+  
 });
+
+var users;
+
+app.get("/toto", async (req, res) => {
+  console.log("Client request received");
+ // const users = await User.find().exec();
+  users =[{name:'tyui'},{name:'tototyu'},{name:'reaesd'}]
+  console.log(users[0].name);
+  res.send(
+    {users}
+  );
+})
+
+app.post("/toto", (req, res) => {
+  var myData = new User(req.body);
+  myData.save()
+  .then(item => {
+  /* res.send("item saved to database") ; */ 
+  })
+  .catch(err => {
+  res.status(400).send("unable to save to database");
+  });
+ });
+
 
 // Setup a record in the database to retrieve
 const { Schema } = mongoose;
@@ -63,5 +104,17 @@ user
   .save()
   .then(user => console.log(`${user.name} saved to the database`))
   .catch(err => console.log(err));
+  
+const user2 = new User({ name: "Toto" });
+  user2
+    .save()
+    .then(user => console.log(`${user.name} saved to the database`
+    
+    
+    ))
+    .catch(err => console.log(err));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    // app.post('/create', function(req, res) {
+    //   const user3 = new User ( {   name: "tata"  }) ) ;
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); // port = 4000
